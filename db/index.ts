9c232c818b07f47b64555af21eaf7d170bbb4a18
Ryan Chenkie <ryanchenkie@gmail.com>
2021-06-28 16:36:27 -0400

    add 02-start

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+// TODO: instantiate Prisma and provide it as a global
 
-// add prisma to the NodeJS global type
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
-}
-
-// Prevent multiple instances of Prisma Client in development
-declare const global: CustomNodeJsGlobal;
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+const prisma = {};
 
 export default prisma;
